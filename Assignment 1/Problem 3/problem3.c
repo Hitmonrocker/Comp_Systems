07ce113d731aa205f273f8ed6727caff9b860fec
ThumTact <35500983+ThumTact@users.noreply.github.com>
2018-03-08 14:00:38 -0500

    Update problem3.c

--- Assignment 1/Problem 3/problem3.c
+++ Assignment 1/Problem 3/problem3.c
@@ -6,39 +6,30 @@ void wait_for_children(pid_t pid);
 void explain_wait_status(pid_t pid, int status);
 pid_t* var;
 
+//Signal Handler for custom signal sigUSR, which has our wait for children function
 void sigUSR_Handler(int signum)
 {
 	printf("Received signal %d\n Wait for Child", signum);
 		return;
-	//wait_for_children("The current node");
-	//	We need to search through the array list to find this
-	//	node in it to continue forward. We have this nodes 
-	//	PID with getpid() so we just have to search through
-	//	the arraylist.
-	//
-	//
 }
 void sigKill_Handler(int signum)
 {
 	printf("Received signal %d\n ", signum);
 		return;
 }
-/*void sigCont_Handler(int signum)
-{
-	printf("Received signal %d\n", signum);
-		return;
-}*/
 
 int main() 
 {
-	int status;
-	signal(SIGUSR1, sigUSR_Handler);
+	int status;  //defining the status for the Signal handlers
+	signal(SIGUSR1, sigUSR_Handler);//defining the signal for it's appropriate handler
+	//print code from problem 2 to display the tree
 	struct tree_node* parentNode;
 	printf("\n");
 	parentNode = read_tree_file(FILE_NAME);
 	printf("\n\nparentNode: %s\n", parentNode->name);
 	return 0;
 }
+//standard function to explain the wait status
 explain_wait_status(pid_t pid, int status)
 {
 	if (WIFEXITED(status))
@@ -52,17 +43,12 @@ explain_wait_status(pid_t pid, int status)
 			(long)pid, WSTOPSIG(status));
 	fflush(stderr);
 }
+//Custom function to wait for children 
  void wait_for_children(pid_t pid){
-	int i = 0;
+ 	//defining the status integer for the waitpid function
 	int status;
+	//parent waits for its children to print creating that depth-first structure
 	waitpid(pid, &status, 0);
+	//explain why the program was waiting or terminated
 	explain_wait_status(pid,status);
-	/*for(i < root->numChildren; i++)
-		{
-			struct tree_node *currentChild = root->pidArr[i];			     
-			wait_for_children(currentChild);
-			kill(currentChild->pid, SIGKILL);
-			explain_wait_status(pid, status);
-		
-		}*/
 	}
