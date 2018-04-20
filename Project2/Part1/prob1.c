a81697948065907d4fbeb69587b19bfe3038a2c0
ThumTact <35500983+ThumTact@users.noreply.github.com>
2018-04-20 16:19:43 -0400

    Update prob1.c

--- Project2/Part1/prob1.c
+++ Project2/Part1/prob1.c
@@ -9,26 +9,26 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <time.h>
-static int p;
+static int p; //int used as identifier for the signal the process receives
+//Function run by the processes.  The signum they receive determines what part of the function runs, min, max, or sum
 int mms(int* s, int i, int size);
+//The handlers grab the signum ints and use them as an identifier for what process to run
 void sigUSR1_Handler(int signum){
-  //printf("\n\nReceived signal %d\n", signum);
   p = signum;
     return;
 }
 void sigUSR2_Handler(int signum){
- // printf("\n\nReceived signal %d\n", signum);
- p = signum;
+  p = signum;
     return;
 }
 void sigCONT_Handler(int signum){
-//  printf("\n\nReceived signal %d\n", signum);
   p = signum;
     return;
 }
 
 int main(int argc, char const *argv[])
 {
+    //necessary to read the numbers.txt file
     FILE* FP;
     signal(SIGUSR1, sigUSR1_Handler);
     signal(SIGUSR2, sigUSR2_Handler);
@@ -50,9 +50,10 @@ int main(int argc, char const *argv[])
     }
     
   fclose(FP);
+  //pid and status variables in arrays to easily keep track of our processes
   pid_t list[4];
   int status[4];
-  
+  //creating the processes
     list[0] = fork();
     if(list[0] == 0)
     {
@@ -67,20 +68,20 @@ int main(int argc, char const *argv[])
                   list[2] = fork();
               if(list[2] == 0)
                 {
-                  kill(getppid(),SIGUSR1);      
+                  kill(getppid(),SIGUSR1); //send SIGUSR 1 to process       
                 }
               else
                 {
                   waitpid(list[2],&status[2],0);
-                   printf("This is the Sum : %d\n",mms(arr,p,atoi(argv[2])));
-                  kill(getppid(),SIGUSR2);
+                   printf("This is the Sum : %d\n",mms(arr,p,atoi(argv[2])));//print the results from mms
+                  kill(getppid(),SIGUSR2);//raises sigUSR2 which determines the sum
                 }
               }
           else
             { 
               waitpid(list[0],&status[0],0);
               printf("This is the Min : %d\n",mms(arr,p,atoi(argv[2])));
-              kill(getppid(),SIGCONT);
+              kill(getppid(),SIGCONT);//raises SIGCONT which triggers the min function
             }
       }
       else
@@ -112,7 +113,7 @@ int mms(int *s,int q,int size)
       {
       for(; i < size; i++)
       {
-        sum += s[i];
+        sum += s[i];//sum all the numbers from the input file
       }
       return sum;
     }
@@ -121,7 +122,7 @@ int mms(int *s,int q,int size)
         {
         for(; i < size; i++ )
         {
-          if(min > s[i])
+          if(min > s[i])//if the min is larger than the current number than min is equal to that current number
             min = s[i];
         }
         return min;
@@ -131,7 +132,7 @@ int mms(int *s,int q,int size)
        {
         for(; i< size; i++ )
         {
-          if(max < s[i])
+          if(max < s[i])//same as min but instead we track if the number is smaller
             max = s[i];
         }
         return max;
